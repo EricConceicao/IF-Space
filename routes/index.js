a03d98e220ab4f066cd929057b032faee81e5400
taticar <carvalho.t@aluno.ifsp.edu.br>
2023-03-30 16:57:44 -0300

    Estrutura básica do projeto

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET página inicial. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+ /* GET página contato. */
+ router.get('/contato', function(req, res, next) {
+  res.render('contato', { title: 'IF - Space | Contato' });
+ });
+ /* GET página equipe. */
+ router.get('/equipe', function(req, res, next) {
+  res.render('equipe', { title: 'IF - Space | Equipe' });
+ });
+ /* GET página sobre. */
+ router.get('/sobre', function(req, res, next) {
+  res.render('sobre', { title: 'IF - Space | Sobre' });
+ });
+module.exports = router;
