038d9ea03212e6194f6a5b8cfc52d6952da3a0c1
Eric <eric.r@aluno.ifsp.edu.br>
2023-04-28 13:39:36 -0300

    Fazendo a seção comentários

--- routes/posts.js
+++ routes/posts.js
@@ -3,13 +3,13 @@ const router = express.Router();
 
 
 //Receber o post do usuário em que você acessou 
-router.get('/:id', (req, res, next) => {
+router.get('/user/:id', (req, res, next) => {
     let id = req.params.id;
     res.render('principal/userpost', { id: id })
 });
 
 //Receber a página para fazer uma postagem
-router.get('/', (req, res, next) => {
+router.get('/:id', (req, res, next) => {
     //Se não carregar o cookie. Ele irá renderizar a página ainda
     let name = typeof req.cookies.name !== 'undefined' ? req.cookies.name : '';
 
