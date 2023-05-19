770aceef52f6d056e3cbdeee901c3578674374cd
Eric <eric.r@aluno.ifsp.edu.br>
2023-05-19 00:12:59 -0300

    Comitando o que não foi comitado <'-'>

--- routes/home.js
+++ routes/home.js
@@ -4,13 +4,23 @@ var router = express.Router();
 
 // Renderiza a página inicial com o nome do usuário
 router.get('/', (req, res, next) => {
-    const pNome = req.session.usuario.pNome;
-    res.render('principal/home', { name: pNome});
+    if (req.session.usuario.nick) {
+        const { nick } = req.session.usuario;
+        res.render('principal/home', { name: nick});
+    } else {
+        const { pNome } = req.session.usuario;
+        res.render('principal/home', { name: pNome});
+    }
 });
 
 router.get('/profile', (req, res, next) => {//Vai ter um parâmetro 'id' aqui depois
-    const pNome = req.session.usuario.pNome;
-    res.render('principal/profile', { name: pNome});
+    if (req.session.usuario.nick) { //Se o usuário tiver um nick. O rendezira primeiro
+        const { nick } = req.session.usuario;
+        res.render('principal/profile', { name: nick});
+    } else {
+        const { pNome } = req.session.usuario;
+        res.render('principal/profile', { name: pNome});
+    }
 });
 
 module.exports = router;
